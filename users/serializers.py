89937f98ae34d2eccb3393e4187b30ea8f71c40b
rawan <rawan.aldemaeen@ibtikar.net.sa>
2021-05-09 01:21:23 +0300

    create UserCreateSerializer with valdation function
    Modify UserUpdate and UserCreate view

--- users/serializers.py
+++ users/serializers.py
@@ -1,24 +1,65 @@
 from django.contrib.auth.password_validation import validate_password
+from django.core import validators
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):  # User GET request serializer
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+
+class CreateUserSerializer(serializers.ModelSerializer):  # User Post request serializer
     username = serializers.CharField(max_length=100)
     password = serializers.CharField(validators=[validate_password],
-                                     style={'input_type': 'password', 'placeholder': 'Password'})
-    email = serializers.CharField(max_length=250)
+                                     style={'input_type': 'password', 'placeholder': 'Password'},
+                                     max_length=30, write_only=True)
+    email = serializers.EmailField(max_length=250, validators=[validators.validate_email])
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'email']
+        fields = ['username', 'password', 'email']
+
+    def validate(self, attrs):
+        password = attrs.get('password', '')
+        email = attrs.get('email', '')
+
+        if len(password) < 8 or len(password) > 30:
+            raise serializers.ValidationError(
+                {'password': ('Password length should be between 8 - 30 characters')}
+            )
+
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError(
+                {'email': ('Email is already in use')}
+            )
+
+        return super().validate(attrs)
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
+        return User.objects.create_user(**validated_data)
+
+
+class UpdateUserSerializer(serializers.ModelSerializer):  # User PUT request serializer
+    username = serializers.CharField(max_length=100)
+    password = serializers.CharField(validators=[validate_password],
+                                     style={'input_type': 'password', 'placeholder': 'Password'},
+                                     max_length=30, write_only=True)
+    email = serializers.EmailField(max_length=250, validators=[validators.validate_email])
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+
+    def validate(self, attrs):
+        password = attrs.get('password', '')
+        if len(password) < 8 or len(password) > 30:
+            raise serializers.ValidationError(
+                {'password': ('Password length should be between 8 - 30 characters')}
+            )
+
+        return super().validate(attrs)
 
     def update(self, instance, validated_data):
         instance.username = validated_data.get('username', instance.username)
@@ -26,5 +67,3 @@ class UserSerializer(serializers.ModelSerializer):
         instance.email = validated_data.get('email', instance.email)
         instance.save()
         return instance
-
-
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics, permissions
 from . import serializers
 from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class UserList(generics.ListAPIView):  # All users list view
@@ -14,15 +16,27 @@ class UserDetails(generics.RetrieveAPIView):    # User detail view
 
 
 class UserCreate(generics.CreateAPIView):   # create new user view
-    queryset = User.objects.all()
-    serializer_class = serializers.UserSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = serializers.CreateUserSerializer
+
+    def post(self, request, **kwargs):
+        serializer = serializers.CreateUserSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class UserUpdate(generics.UpdateAPIView):   # Update user data view
-    queryset = User.objects.all()
-    serializer_class = serializers.UserSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = serializers.UpdateUserSerializer
+
+    def update(self, request, *args, **kwargs):
+        serializer = self.serializer_class(request.user, data=request.data, partial=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class UserDestroy(generics.DestroyAPIView):     # Delete user view
